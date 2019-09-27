06d232af97e0a0aa12b6257904971b8925fd52a8
Maria may Roberts <mariamayr96@gmail.com>
2019-09-26 23:23:33 -0700

    index.html

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,22 +1,8 @@
- $(document).ready(function() {
-
-    //Create a variables name topics and assigned with disney characters in string.
-      var topic = ["cinderella", "goofy", "tinkerbell", "elsa", "ariel", "pooh", "belle", "simba"];
+  //Create a variables name topics and assigned with disney characters in string.
+      var topics = ["cinderella", "goofy", "tinkerbell", "elsa", "ariel", "pooh", "belle", "simba"];
  
-      renderButtons();
-     function renderButtons() {
-              
-        $("#disney-characters").empty();
-        for (var i = 0; i < topic.length; i++) { 
-         var a = $("<button>");
-          a.addClass("newCharacter");
-          a.attr("data-name", topic[i]);
-          a.text(topic[i]);
-          $("#disney-characters").append(a);
-      }
-  }
-   //Adding click  event listen listener to all buttons
-  $("button").on("click", function(event) {
+   //disneyCharacterInfo function re-render the HTML to display the appropriate content
+     function disneyCharacterInfo() {
       event.preventDefault();
         var topic = $(this).attr("data-name");
            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=iNztTxLueglvaGFJ6pTOQUtn4vLGftx6&limit=10";
@@ -35,7 +21,7 @@
                //Using for loop for the results on whatever the responce that return.
              for (var i = 0; i < results.length; i++) {
                
-               var gifDiv = $("<div>");
+               var gifDiv = $("<div class='topic'>");
                var p = $("<h5>").text("Rating: " + "" + rating);
 
               var rating = results[i].rating;
@@ -50,23 +36,40 @@
                  $("#gifs-box").prepend(gifDiv);
             };
          });
-     });  
+     }
+     //function for displaying the disneyCharacters data
+     function renderButtons() {
+              
+      $("#disney-characters").html("");
+      for (var i = 0; i < topics.length; i++) { 
+       var a = $("<button>");
+        a.addClass("disney-btn");
+        a.attr("data-name", topics[i]);
+        a.text(topics[i]);
+        $("#disney-characters").append(a);
+    };
+}
+   //The function that handles event where diney-input added
+   $("#find-disneyCharacter").on("click", function(event) {
+     event.preventDefault();
 
-     $("#find-disneyCharacter").on("click", function(event) {
-      event.preventDefault();
+     var topic = $("#disney-input").val().trim();
 
-      var newCharacter = $("#disney-input").val().trim();
- 
-       topic.push(newCharacter);
-      // console.log(topic);
-        renderButtons();  
-      });
+       topics.push(topic);
+     // console.log(topic);
+     renderButtons();  
+     });
+   
+     $(document).on('click', ".disney-btn", disneyCharacterInfo);
+                 
+     renderButtons();
 
-      
-     
-        $("#gifs-box").on("click", "img", function() {
+    //The jquery method that allow to get the value of attr from HTML element
+      $("#gifs-box").on("click", "img", function() {
             var state = $(this).attr("data-state");
-            
+        /// If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value   
            if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
            $(this).attr("data-state", "animate");
@@ -77,9 +80,7 @@
        
            }
       }); 
-         
- });
-    
 
+     
 
          
